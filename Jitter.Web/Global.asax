4400a02f0528bbe75a9f14c37bb8a1757564762b
Zach Ballard <zbginji@gmail.com>
2016-04-21 13:26:19 -0500

    Add project files.

---
+++ Jitter.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jitter.Web.MvcApplication" Language="C#" %>
---
+++ Jitter.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jitter.Web.Startup))]
+namespace Jitter.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
